f63d72987c5f2e90f0688579af58ae97ce5fd4e0
r4scodee <irbadhganzz@gmail.com>
2025-09-11 02:22:04 +0700

    check db

--- Database/db_fitcom.sql
+++ Database/db_fitcom.sql
@@ -1,11 +1,23 @@
-CREATE DATABASE smart_farm_db;
-USE smart_farm_db;
+-- --------------------------------------------------------
+-- Host:                         103.163.138.166
+-- Server version:               10.6.22-MariaDB-cll-lve - MariaDB Server
+-- Server OS:                    Linux
+-- HeidiSQL Version:             12.8.0.6908
+-- --------------------------------------------------------
 
-CREATE TABLE products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    code VARCHAR(50) NOT NULL UNIQUE,  -- Kode unik produk
-    name VARCHAR(100) NOT NULL,        
-    price DECIMAL(10,2) NOT NULL,      
-    image VARCHAR(255) DEFAULT NULL,  
-    supplier VARCHAR(100) NOT NULL     
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
